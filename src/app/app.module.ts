46c6584e49e8a9249d4c7662396a46b5291f3e2e
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-10 23:23:03 +0300

    patch: It will add components and basic html for main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BannerComponent } from './banner/banner.component';
+import { QuotesListComponent } from './quotes-list/quotes-list.component';
+import { AddQuoteComponent } from './add-quote/add-quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    BannerComponent,
+    QuotesListComponent,
+    AddQuoteComponent
   ],
   imports: [
     BrowserModule,

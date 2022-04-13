30222b466803d95d0b5c97dac9a8d4b5a88e76db
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-13 13:29:49 +0300

    patch: It will set quotes to be generated from the component

--- src/app/quotes-list/quotes-list.component.ts
+++ src/app/quotes-list/quotes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes-list',
@@ -11,5 +12,12 @@ export class QuotesListComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  quotes: Quotes[] = [
+    {id: 1, author: 'Edward kevin', quote: 'This is quote number one', user: 'Ian', upvote: 0, downvote: 0},
+    {id: 2, author: 'john kevin', quote: 'This is quote number two', user: 'leon', upvote: 0, downvote: 0},
+    {id: 3, author: 'Doe kevin', quote: 'This is quote number three', user: 'dan', upvote: 0, downvote: 0},
+    {id: 4, author: 'Gerald kevin', quote: 'This is quote number four', user: 'fello', upvote: 0, downvote: 0},
+  ];
 
 }

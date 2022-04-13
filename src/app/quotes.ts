51dd9ec494398e385847f6bd18ea957eac60e477
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-13 20:51:25 +0300

    patch: It will add constructor for quotes class

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -2,8 +2,19 @@ export class Quotes {
     id?: number;
     author?: string;
     quote?: string;
+    dateAdded?: Date;
     user?: string;
-    upvote?: number;
-    downvote?: number;
+    upvote: number = 0;
+    downvote: number = 0;
+
+    constructor(id: number, author: string, quote: string, dateAdded: Date, user: string, upvote: number, downvote: number){
+        this.id = id;
+        this.author = author;
+        this.quote = quote;
+        this.dateAdded = dateAdded;
+        this.user = user;
+        this.upvote = upvote;
+        this.downvote = downvote;
+    }
 
 }

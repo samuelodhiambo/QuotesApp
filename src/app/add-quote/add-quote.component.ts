3e499d00bdf1e116a62290b29189c1ced79572f5
Samuel Odhiambo <samuel.odhiambo@student.moringaschool.com>
2022-04-13 20:49:01 +0300

    patch: It will add output binding for quote submission

--- src/app/add-quote/add-quote.component.ts
+++ src/app/add-quote/add-quote.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quotes } from '../quotes';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-quote',
@@ -6,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-quote.component.css']
 })
 export class AddQuoteComponent implements OnInit {
+  newQuote = new Quotes(0, '', '', new Date(), '', 0, 0)
+
+  @Output() addQuote = new EventEmitter<Quotes>();
+
+  submitQuote(myform: NgForm){
+    this.addQuote.emit(this.newQuote);
+    // myform.reset();
+  }
 
   constructor() { }
 
